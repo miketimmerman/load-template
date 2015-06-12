315af02cb61da5a39efd74ef397282afb6ea70aa
miketimmerman <miketimmerman@gmail.com>
2015-06-12 13:51:45 +0200

    first commit
    first commit

---
+++ loadTemplate.js
@@ -0,0 +1,38 @@
+var LoadTemplate = function(element, template, data){
+    // Check if parenet element is defined as string or object.
+    if(typeof element == 'string'){
+        this.el = document.getElementById(element);
+    } else {
+        this.el = element;
+    }
+
+    // Store template name and data.
+    this.tempName = template;
+    this.data = data || null;
+
+    // You can change this to path of your template folder.
+    this.folderPath = 'assets/templates/';
+};
+
+LoadTemplate.prototype.create = function(callback){
+    var req = new XMLHttpRequest();
+    var that = this;
+
+    // Define parameters for request.
+    req.open('get', this.folderPath + this.tempName + '.handlebars', true);
+
+    // Wait for request to complete.
+    req.onreadystatechange = function(){
+        if (req.readyState == 4 && req.status == 200){
+            //Compile HB template, add data (if defined) and place in parent element.
+            var compiled = Handlebars.compile(req.response);
+            that.el.innerHTML = compiled(that.data);
+
+            // Execute callback function
+            callback();
+        }
+    };
+
+    // Send request.
+    req.send();
+};
